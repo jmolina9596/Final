075086c47e7d3e5d42d858ea2d173d4effa8f26b
jmolina9596 <jmolina9596@yahoo>
2018-05-15 23:57:01 -0700

    changes

--- public/javascripts/edit_user.js
+++ public/javascripts/edit_user.js
@@ -4,21 +4,18 @@ function updateUser() {
   const url = window.location.pathname;
   const userId = url.substring(url.lastIndexOf("/") + 1);
   console.log(userId);
-  const salaryVal = $("input[name=salary]")
+  const phoneNumber = $("input[name=phone]")
     .val()
     .trim();
-  const salary = parseInt(salaryVal, 10);
+  const phone = parseInt(phoneNumber, 10);
   const user = {
     name: $("input[name=name]")
       .val()
       .trim(),
-    address: $("textarea[name=address]")
+    email: $("input[name=email]")
       .val()
       .trim(),
-    position: $("input[name=position]")
-      .val()
-      .trim(),
-    salary
+    phone
   };
 
   const request = $.ajax({
